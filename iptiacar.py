c68ec883cfa48147597dfc956f26f86acd227c63
mnoriel08 <110777120+mnoriel08@users.noreply.github.com>
2022-08-25 20:17:29 +0100

    Add files via upload

--- iptiacar.py
+++ iptiacar.py
@@ -10,23 +10,17 @@
 #=======================================================================
 # seccion 1 - importacion de librerias y modulos de Python
 #=======================================================================
-from picarx import Picarx # funcionalidad basica del iptiacar
+from picarx    import Picarx # funcionalidad basica del iptiacar
 import time            # modulo p/acceder a funciones de fechas y tiempo
-import cv2			   # cv2 = OpenCV - biblioteca p/ visión artificial
-from robot_hat import TTS # biblioteca p/convertir texto a voz
-from picamera import PiCamera # Interfaz Python p/cámara Raspberry Pi
-from picamera.array import PiRGBArray
-from vilib import Vilib # biblioteca p/procesamiento visual de imágenes
-                        # con funciones como reconocimiento de color,
-                        # reconocimiento facial, detección de manos,
-                        # clasificación imágenes, detección de objetos,
-                        # transmisión inalámbrica de video, etc.
+from vilib     import Vilib  # reconocimiento de color y de objetos
+from robot_hat import TTS    # sintetizar textos a voz
+from grayscale_module import Grayscale_Module # 
 #=======================================================================
 # seccion 2 - variables globales
 #=======================================================================
 
 # parametros del juego o carrera
-juegoSentido      = 0           # 0 sentido del reloj/1 contrareloj 
+juegoDireccion    = ""          # 0 sentido del reloj/1 contrareloj 
 juegoMaxVueltas   = 3
 juegoTiempo       = 0           # duracion en segundos (cronometro)
 juegoTiempoMax    = 180         # limite maximo del juego en segundos
@@ -36,7 +30,7 @@ juegoTTS          = ""          # texto para convertirlo a voz
 # parametros de la pista
 pistaX            = 2.98        # dimension X en metros
 pistaY            = 2.98        # dimension Y en metros
-#pistaColorBorde   = black
+pistaColorObjeto  = ""          # color de objeto encontrado en la pista
 #pistaColorPiso    = white
 pistaLineaNaranja = (0,60,100,0)
 pistaLineaAzul    = (100,80,0,0)
@@ -58,6 +52,7 @@ autoDistancia     = 0           # distancia del auto-robot al obstaculo
 #=======================================================================
 # seccion 3 - funciones
 #=======================================================================
+ix = Picarx() # crea instancia del objeto auto-robot iptiacar
 
 # mover auto en varias direcciones y detenerlo
 def moverAuto(operador, autoVelocidad): 
@@ -100,24 +95,80 @@ def esquivarDerecha(autoAngulo):
 		ix.set_dir_servo_angle(0)
 		ix.forward(autoVelocidad)
 
-# emitir textos en sonidos
-def texto_a_voz(juegoTTS):
-	for i in juegoTTS:
-		ttsRobot.say(i)
+# detectar color que esta viendo el auto-robot al frente
+def detectarColor(pistaColorObjeto):
+    Vilib.camera_start(vflip=False,hflip=False)
+    Vilib.color_detect(pistaColorObjeto) # color a detectar en objeto 
+    time.sleep(2)
+
+    while True:
+        try:
+            num = Vilib.detect_obj_parameter['color_n'] 
+            time.sleep(0.5)
+        except Exception as e:
+            print('error:%s'%e)
+            break
+    Vilib.camera_close()
+    return num
 
 def detectarObstaculo():
 	while True:
 		autoDistancia = ix.ultrasonic.read()
 		if autoDistancia > 0 and autoDistancia < 300:
-			if autoDistancia < 30 and detectarColor() == "rojo":
-				esquivarIzquierda()
+			if autoDistancia < 30 and detectarColor("red") != 0:
+				esquivarDerecha()   # obstaculo era rojo
 			else:
-				esquivarDerecha()
+				esquivarIzquierda() # obstaculo era verde
 
-def main():  # bucle principal
-	time.sleep(2) # esperar a que arranque
-	print("iptiacar esta listo")
-	detectarObstaculo()
+def textAvoz(juegoTTS):
+	tts_robot = TTS()
+	for i in juegoTTS:
+		print(i)
+		tts_robot.say(i)
+
+def detectar_esquina():
+	try:
+		while True:
+			gm_val_list = ix.get_grayscale_data()
+			print("gm_val_list:",gm_val_list)
+			gm_status = ix.get_line_status(gm_val_list)
+			print("gm_status:",gm_status)
+
+			if gm_status == 'forward':
+				print(1)
+				ix.forward(autoVelocidad) 
+
+			elif gm_status == 'left':
+				ix.set_dir_servo_angle(12)
+				ix.forward(autoVelocidad) 
+
+			elif gm_status == 'right':
+				ix.set_dir_servo_angle(-12)
+				ix.forward(autoVelocidad) 
+			else:
+				ix.set_dir_servo_angle(0)
+				ix.stop()
+	finally:
+		px.stop()
+
+def main():  # bucle principal del algoritmo
+	time.sleep(2) # esperar a que arranque del programa
+	print("iptiacar esta listo...")
+	print("Detectar borde de pista frente al auto-robot...")
+
+	# calcular a que distancia esta el borde negro de la pista
+	if detectarColor("red") == 0: # detectar color rojo ?
+		# no detecto el color rojo
+		if detectarColor("green") == 0: # detectar color verde ?
+			# no detecto el color verde, se asume color negro
+			autoDistancia = ix.ultrasonic.read() # mide distancia/borde
+			print("Distancia al borde de pista fue: " + autoDistancia)
+		else:
+			print("Auto-robot mal ubicado en posicion de salida...")
+			for i in range(0,2):
+				juegoTTS = ["Auto","robot","mal","ubicado","en","posicion","de","salida"]
+			ix.stop()
+
 
 #=======================================================================
 # seccion 4 - ventana principal del interfaz grafico de usuario (GUI)
@@ -129,8 +180,6 @@ def main():  # bucle principal
 
 if __name__ == "__main__":
 	try:
-		ix = Picarx()             # instancia objeto auto-robot iptiacar
-		ttsRobot = TTS()          # instancia objeto robot_hat
 		main()
 	except Exception as e:
 			print("error:s"%e)
